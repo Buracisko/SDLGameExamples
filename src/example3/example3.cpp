42677a79f5f6741721f457aea98f532a01654b7e
Jakub Bureš <jak.bures@gmail.com>
2023-05-22 15:23:12 +0200

    Refactoring and bug fixing of AABB collisions

--- src/example3/example3.cpp
+++ src/example3/example3.cpp
@@ -16,8 +16,34 @@ void RenderFrame(float dt);
 #define WINDOW_WIDTH 640
 #define WINDOW_HEIGHT 360
 
-#define NO_RECTS 2
+#define NO_RECTS 15
 SDL_Rect rects[NO_RECTS];
+
+void SetupRects()
+{
+	rects[0] = SDL_Rect {128, 128, 64, 64};
+	rects[1] = SDL_Rect {256, 128, 64, 64};
+
+	rects[2] = SDL_Rect {128, 256, 32, 32};
+
+	rects[3] = SDL_Rect {128 + 32, 256, 32, 32};
+	rects[4] = SDL_Rect {128 + 64, 256, 32, 32};
+	rects[5] = SDL_Rect {128 + 96, 256, 32, 32};
+
+	rects[6] = SDL_Rect {512, 80, 16, 64};
+	rects[7] = SDL_Rect {512, 80 + 64, 16, 64};
+	rects[8] = SDL_Rect {512, 80 + 128, 16, 64};
+	rects[9] = SDL_Rect {512, 80 + 192, 16, 64};
+
+	rects[10] = SDL_Rect {16, 16, 128, 32};
+	rects[11] = SDL_Rect {16 + 128, 16, 128, 32};
+	rects[12] = SDL_Rect {16 + 256, 16, 128, 32};
+	rects[13] = SDL_Rect {16 + 384, 16, 128, 32};
+
+	rects[14] = SDL_Rect {256, 256 + 64, 128, 128};
+}
+
+SDL_Rect playerRect = {192, 64, 64, 64};
 
 //=============================================================================
 int main(int argc, char* argv[])
@@ -32,8 +58,7 @@ int main(int argc, char* argv[])
 		return 1;
 	}
 
-	rects[0] = SDL_Rect {0, 0, 64, 64};
-	rects[1] = SDL_Rect {255, 128, 128, 128};
+	SetupRects();
 
 	// Push functions to the game loop
 	StartLoop(Update, RenderFrame);
@@ -44,9 +69,26 @@ int main(int argc, char* argv[])
 
 //=============================================================================
 
-struct Vec2 {
+struct Vector2 {
 	float x, y;
 };
+
+Vector2 AddVector2(const Vector2 v1, const Vector2 v2)
+{
+	return Vector2 {v1.x + v2.x, v1.y + v2.y};
+}
+
+Vector2 SubVector2(const Vector2 v1, const Vector2 v2)
+{
+	return Vector2 {v1.x - v2.x, v1.y - v2.y};
+}
+
+Vector2 MultVector2(const Vector2 v, float val)
+{
+	return Vector2 {v.x * val, v.y * val};
+}
+
+//=============================================================================
 
 void swapf(float* a, float* b)
 {
@@ -67,30 +109,26 @@ float minf(float a, float b)
 
 // Source: https://github.com/OneLoneCoder/Javidx9/blob/master/PixelGameEngine/SmallerProjects/OneLoneCoder_PGE_Rectangles.cpp
 
-bool RayVsRect(const Vec2* rayOrigin, const Vec2* rayDir,
-	const SDL_Rect* target, Vec2* contactPoint, Vec2* contactNormal,
+bool RayVsRect(const Vector2* rayOrigin, const Vector2* rayDir,
+	const SDL_Rect* target, Vector2* contactPoint, Vector2* contactNormal,
 	float* tHitNear
 )
 {
-	//TODO: Change SDL_Rect to SDL_FRect. My linux libraries are ancient
-	*contactNormal = {0 , 0};
-	*contactPoint = {0, 0};
-
-	// Cache division
-	Vec2 invDir = {1.0f / rayDir->x, 1.0f / rayDir->y};
+	// Cache division to multiply instead of divide later
+	Vector2 invDir = {1.0f / rayDir->x, 1.0f / rayDir->y};
 
 	// Calculate intersections with rectangle bounding axes
-	Vec2 tNear = {
+	Vector2 tNear = {
 		((float)target->x - rayOrigin->x) * invDir.x,
-		((float)target->y - rayOrigin->y) * invDir.y
+	 	((float)target->y - rayOrigin->y) * invDir.y
 	};
-	Vec2 tFar = {
+	Vector2 tFar = {
 		((float)(target->x + target->w) - rayOrigin->x) * invDir.x,
 		((float)(target->y + target->h) - rayOrigin->y) * invDir.y
 	};
 
-	if (tFar.x == NAN || tFar.y == NAN) return false;
-	if (tNear.x == NAN || tNear.y == NAN) return false;
+	if (isnan(tFar.x) || isnan(tFar.y)) return false;
+	if (isnan(tNear.x)|| isnan(tNear.y)) return false;
 
 	// Sort distances
 	if (tNear.x > tFar.x) swapf(&tNear.x, &tFar.x);
@@ -113,7 +151,7 @@ bool RayVsRect(const Vec2* rayOrigin, const Vec2* rayDir,
 	contactPoint->x = rayOrigin->x + *tHitNear * rayDir->x;
 	contactPoint->y = rayOrigin->y + *tHitNear * rayDir->y;
 
-	// Make better normal vectors
+	// Make better normal vectors using dot product
 	if (tNear.x > tNear.y)
 	{
 		if (invDir.x < 0)
@@ -135,6 +173,42 @@ bool RayVsRect(const Vec2* rayOrigin, const Vec2* rayDir,
 	return true;
 }
 
+bool RectVsRectCollision(const SDL_Rect* movingRect, const SDL_Rect* staticRect, Vector2* displacement, Vector2* contactPoint, Vector2* contactNormal, float* contactTime)
+{
+	// Check if rectangle is actually moving - we assume rectangles are NOT in collision to start
+	if (displacement->x == 0 && displacement->y == 0)
+	{
+		return false;
+	}
+
+	*contactPoint = {};
+	*contactNormal = {};
+	*contactTime = .0f;
+
+	// Expand target rectangle by source dimensions
+	SDL_Rect expandedTargetRect = *staticRect;
+	expandedTargetRect.x -= playerRect.w / 2;
+	expandedTargetRect.y -= playerRect.h / 2;
+	expandedTargetRect.w += playerRect.w;
+	expandedTargetRect.h += playerRect.h;
+
+	Vector2 positionCenter = {
+		(float)(movingRect->x + movingRect->w / 2),
+		(float)(movingRect->y + movingRect->h / 2)
+	};
+
+	// Are we colliding with the target rect?
+	if (RayVsRect(&positionCenter, displacement, &expandedTargetRect, contactPoint, contactNormal, contactTime))
+	{
+		if (*contactTime >= 0 && *contactTime <= 1.0f)
+		{
+			return true;
+		}
+	}
+
+	return false;
+}
+
 void Update(float dt)
 {
 	// Change subsystem of project from Windows to Console
@@ -142,12 +216,8 @@ void Update(float dt)
 	if (IsKeyDown(SDL_SCANCODE_ESCAPE))
 		ExitGame();
 
-	const float speed = 100;
-	Vec2 displacement = {0, 0};
-	Vec2 positionCenter = {
-		(float)(rects[0].x + rects[0].w / 2),
-		(float)(rects[0].y + rects[0].h / 2)
-	};
+	const float speed = 150;
+	Vector2 displacement = {0, 0};
 
 	if (IsKeyDown(SDL_SCANCODE_RIGHT))
 		displacement.x += speed * dt;
@@ -159,42 +229,37 @@ void Update(float dt)
 	else if (IsKeyDown(SDL_SCANCODE_UP))
 		displacement.y -= speed * dt;
 
-	// Check if rectangle is actually moving - we assume rectangles are NOT in collision to start
-	if (displacement.x != 0 || displacement.y != 0)
+	for (int i = 0; i < NO_RECTS; ++i)
 	{
-		// Expand target rectangle by source dimensions
-		SDL_Rect expandedTargetRect = rects[1];
-		expandedTargetRect.x -= rects[0].w / 2;
-		expandedTargetRect.y -= rects[0].h / 2;
-		expandedTargetRect.w += rects[0].w;
-		expandedTargetRect.h += rects[0].h;
-
 		// Collision information
-		Vec2 contactPoint = {};
-		Vec2 contactNormal = {};
+		Vector2 contactPoint = {};
+		Vector2 contactNormal = {};
 		float contactTime = .0f;
 
-		// Are we colliding with the target rect?
-		if (RayVsRect(&positionCenter, &displacement, &expandedTargetRect, &contactPoint, &contactNormal, &contactTime)
-			&& (contactTime >= 0.0f && contactTime < 1.0f)
-		)
+		if (RectVsRectCollision(&playerRect, &rects[i], &displacement, &contactPoint, &contactNormal, &contactTime))
 		{
-			// Resolve if we have a normal
-			if (contactNormal.x != 0 || contactNormal.y != 0)
+			// If diagonal colllision (normal == {0, 0}), just stop
+			if (contactNormal.x == 0 && contactNormal.y == 0)
+			{
+				displacement.x = displacement.y = 0;
+			}
+			else
 			{
 				displacement.x += contactNormal.x * fabsf(displacement.x) * (1 - contactTime);
 				displacement.y += contactNormal.y * fabsf(displacement.y) * (1 - contactTime);
 			}
-			// Else no displacement
-			else
-			{
-				displacement.x = displacement.y = 0;
-			}
 		}
 	}
 
-	rects[0].x += (int)(displacement.x);
-	rects[0].y += (int)(displacement.y);
+	playerRect.x += (int)round(displacement.x);
+	playerRect.y += (int)round(displacement.y);
+}
+
+void RenderBox(const SDL_Rect* rect)
+{
+	SDL_RenderDrawRect(gRenderer, rect);
+	SDL_RenderDrawLine(gRenderer, rect->x, rect->y, rect->x + rect->w - 1, rect->y + rect->h - 1);
+	SDL_RenderDrawLine(gRenderer, rect->x + rect->w - 1, rect->y, rect->x, rect->y + rect->h - 1);
 }
 
 void RenderFrame(float interpolation)
@@ -203,9 +268,11 @@ void RenderFrame(float interpolation)
 	SDL_SetRenderDrawColor(gRenderer, 0x3a, 0x2b, 0x3b, 255);
 	SDL_RenderClear(gRenderer);
 
+	// Draw playerRect
+	SDL_SetRenderDrawColor(gRenderer, 0x11, 0x11, 0xff, 255);
+	RenderBox(&playerRect);
 	// Draw rects
 	SDL_SetRenderDrawColor(gRenderer, 0x00, 0xff, 0x11, 255);
-
 	for (int i = 0; i < NO_RECTS; ++i)
-		SDL_RenderFillRect(gRenderer, &rects[i]);
+		RenderBox(&rects[i]);
 }
