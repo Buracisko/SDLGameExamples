51dfae64d5970d1be666a1917faf7fb9400c205a
Buráčisko <jak.bures@gmail.com>
2024-02-05 23:00:57 +0100

    Yet another reorganization of project structure

--- configure.bat
+++ configure.bat
@@ -6,7 +6,9 @@ cmake -S . -B ./build ^
 -DSDL2_MIXER_PATH="libraries/SDL2_mixer-2.6.3" ^
 -DSDL2_TTF_PATH="libraries/SDL2_ttf-2.20.2"
 
-mklink /j "build/assets" "./assets"
-
+echo Creating a asset junction
+set assetJunction="build/assets"
+@rem I would like to check if the assetJunction is a junction, but effort is too high
+@rem https://stackoverflow.com/questions/18883892/batch-file-windows-cmd-exe-test-if-a-directory-is-a-link-symlink
+if not exist %assetJunction% mklink /j %assetJunction% "./assets"
 echo Solution ready
-pause
